6091e422ffaae05a8427ee814a6224c6f4f7fc55
imennfaidh8@gmail.com <imennfaidh8@gmail.com>
2025-04-11 12:00:40 +0200

    modification de fraud detection

--- Fraud-Detection-service/src/main/java/com/example/transaction_service/repository/ITransactionRepository.java
+++ Fraud-Detection-service/src/main/java/com/example/transaction_service/repository/ITransactionRepository.java
@@ -4,6 +4,10 @@ import com.example.transaction_service.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ITransactionRepository extends JpaRepository<Transaction,Long> {
+    List<Transaction> findByBankAccount_User_UserIdAndTransactionDateAfter(Long userId, Date afterDate);
 }
--- Fraud-Detection-service/src/main/java/com/example/transaction_service/service/serviceImp/ProcessService.java
+++ Fraud-Detection-service/src/main/java/com/example/transaction_service/service/serviceImp/ProcessService.java
@@ -1,5 +1,10 @@
 package com.example.transaction_service.service.serviceImp;
 
+import com.example.transaction_service.entity.Transaction;
+import com.example.transaction_service.entity.User;
+import com.example.transaction_service.exception.NotFoundException;
+import com.example.transaction_service.repository.ITransactionRepository;
+import com.example.transaction_service.repository.IUserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,38 +18,59 @@ import java.util.Map;
 public class ProcessService {
     @Autowired
     private KafkaProducer kafkaProducer;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+   private ITransactionRepository transactionRepository;
+    @Autowired
+    private IUserRepository userRepository;
 
     public void startProcess(Map<String, Object> data)  {
         try {
             log.info(" Processus déclenché avec données : {}", data);
 
-            // Extraire les champs nécessaires
             Integer transactionIdInteger = (Integer) data.get("fds004_transaction_id");
             Long transactionId = transactionIdInteger != null ? transactionIdInteger.longValue() : null;
             Double amount = (Double) data.get("fds004_amount");
 
-            // Règle de détection de fraude basée sur le montant
+            Transaction transaction = transactionRepository.findById(transactionId)
+                .orElseThrow(() -> new NotFoundException("Transaction not found"));
+
+            User user = transaction.getBankAccount().getUser();
+            Long userId = user.getUserId();
+
+
+            // Règle 1
             boolean isFraudulent = false;
             String reason = "Transaction valide";
-
             if (amount > 3000) {
                 isFraudulent = true;
                 reason = "Montant supérieur à 3000";
             }
+            // Règle 2
+
 
             if (!isFraudulent) {
                 log.info("Transaction non frauduleuse, pas d'envoi dans Kafka.");
                 return;
             }
+            user.setSuspicious_activity(true);
+            userRepository.save(user);
+            Map<String, Object> userMap = new HashMap<>();
+            userMap.put("userId", user.getUserId());
+            userMap.put("firstName", user.getFirstName());
+            userMap.put("lastName", user.getLastName());
+            userMap.put("email", user.getEmail());
+            userMap.put("tel", user.getTel());
+            userMap.put("suspiciousActivity", user.getSuspicious_activity());
 
             // Construire le résultat
             Map<String, Object> fraudResult = new HashMap<>();
-            fraudResult.put("transaction_id", transactionId);
-            fraudResult.put("is_fraudulent", isFraudulent);
-            fraudResult.put("reason", reason);
+
+            fraudResult.put("transactionId", transaction.getTransactionId());
+            fraudResult.put("amount", transaction.getAmount());
+            fraudResult.put("country", transaction.getCountry());
+            fraudResult.put("user", userMap);
 
 
             String fraudResultJson = objectMapper.writeValueAsString(fraudResult);
