7af56f131bf9bc5c17cef7d4add55361b0262ba4
imennfaidh8@gmail.com <imennfaidh8@gmail.com>
2025-06-24 16:27:51 +0200

    correction communication entre container

--- eureka-service/src/main/java/com/example/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/example/eurekaservice/EurekaServiceApplication.java
@@ -1,18 +1,11 @@
 package com.example.eurekaservice;
 
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-
-
 public class EurekaServiceApplication {
 
 
