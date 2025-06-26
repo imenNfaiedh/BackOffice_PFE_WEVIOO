fffb849d18ed91ce7c004371c7e895b41b8717d0
imennfaidh8@gmail.com <imennfaidh8@gmail.com>
2025-06-26 12:43:17 +0200

    docker url , default

--- init-db.sql
+++ init-db.sql
@@ -3,5 +3,13 @@
 CREATE USER "wevioo" with password 'Wevioo@2024++';
 CREATE DATABASE "pfe_db";
 GRANT ALL PRIVILEGES ON DATABASE "pfe_db" to "wevioo";
+
 GRANT ALL ON SCHEMA public TO wevioo;
 GRANT CREATE ON SCHEMA public TO wevioo;
+
+
+
+CREATE USER debezium WITH REPLICATION PASSWORD 'debezium';
+\c pfe_db
+GRANT SELECT ON public.fds004t_transaction TO debezium;
+CREATE PUBLICATION debezium_publication FOR TABLE public.fds004t_transaction;
